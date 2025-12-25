5ddc8387bc6ced68a7875ffc7aae4ed624f658e8
felixjaschul <felix@jaschul.de>
2025-12-25 01:07:20 +0100

    .

--- src/main/java/project/PROJECT.java
+++ src/main/java/project/PROJECT.java
@@ -1,42 +1,84 @@
 package project;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.system.AppSettings;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.ColorRGBA;
-import com.jme3.renderer.RenderManager;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import com.jme3.material.Material;
 
-/**
- * This is the Main Class of your Game. It should boot up your game and do initial initialization
- * Move your Logic into AppStates or Controls or other java classes
- */
-public class PROJECT extends SimpleApplication {
+public class PROJECT extends SimpleApplication implements ActionListener {
+
+    private EDITOR editorState;
+    private STATE gameState;
+    private BulletAppState bulletAppState;
+    private Node levelNode;
 
     public static void main(String[] args) {
         PROJECT app = new PROJECT();
+        AppSettings settings = new AppSettings(true);
+        settings.setTitle("Quake-like Engine");
+        settings.setResolution(1280, 720);
+        app.setSettings(settings);
         app.start();
     }
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/fakeLighting.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
+        levelNode = new Node("LevelNode");
+        rootNode.attachChild(levelNode);
 
-        rootNode.attachChild(geom);
+        // Floor to draw on
+        Box floorBox = new Box(150, 0.1f, 150);
+        Geometry floor = new Geometry("Floor", floorBox);
+        Material floorMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        floorMat.setColor("Color", ColorRGBA.DarkGray);
+        floor.setMaterial(floorMat);
+        // Move floor down so top is at -0.1f, grid is at 0 to avoid Z-fighting
+        floor.setLocalTranslation(0, -0.2f, 0);
+        rootNode.attachChild(floor);
+        
+        // Add floor to physics
+        RigidBodyControl floorPhys = new RigidBodyControl(0);
+        floor.addControl(floorPhys);
+        bulletAppState.getPhysicsSpace().add(floorPhys);
+
+        editorState = new EDITOR(levelNode, bulletAppState);
+        gameState = new STATE(levelNode, bulletAppState);
+
+        stateManager.attach(gameState);
+
+        // Start in game mode
+        stateManager.detach(editorState);
+
+        inputManager.addMapping("ToggleMode", new KeyTrigger(KeyInput.KEY_F1), new KeyTrigger(KeyInput.KEY_F2));
+        inputManager.addListener(this, "ToggleMode");
+        
+        flyCam.setMoveSpeed(20f);
+        cam.setLocation(new Vector3f(0, 5, 10));
+        cam.lookAt(Vector3f.ZERO, Vector3f.UNIT_Y);
     }
 
     @Override
-    public void simpleUpdate(float tpf) {
-        //this method will be called every game tick and can be used to make updates
-    }
-
-    @Override
-    public void simpleRender(RenderManager rm) {
-        //add render code here (if any)
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals("ToggleMode") && isPressed) {
+            if (stateManager.hasState(editorState)) {
+                stateManager.detach(editorState);
+                stateManager.attach(gameState);
+            } else {
+                stateManager.detach(gameState);
+                stateManager.attach(editorState);
+            }
+        }
     }
 }
